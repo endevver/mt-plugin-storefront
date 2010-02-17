b38df6cf4abc9ed5d3a120505be81b5e225fff84
Byrne Reese <byrne@majordojo.com>
2010-02-17 11:45:27 -0800

    initial checkin of IPN handlers for subscriptions

--- plugins/StoreFront/lib/StoreFront/PayPal/App.pm
+++ plugins/StoreFront/lib/StoreFront/PayPal/App.pm
@@ -61,6 +61,9 @@ sub validate {
 
     #$logger->debug("Posting the following content: $content");
 
+    # TODO - validate the receiver is registered to user
+    # TODO - verify the item ordered, its price, etc, correspond to the correct amounts
+
     my $res = $app->{ua}->request($req);    
     if ($res->is_success) {
 	if (trim($res->content) eq 'VERIFIED') {
@@ -73,6 +76,7 @@ sub validate {
     } else {
 	$logger->debug( "PayPal returned an error: " . $res->status_line );
     }
+    # TODO - trigger email
     return 0;
 }
 
@@ -80,17 +84,26 @@ sub handle {
     my $app = shift;
     $logger->debug('Entering "handle"...');
 
+    my $out = ''; # Paypal does not expect response content
     unless (validate( $app )) {
 	MT->log({
 	    message => "There was an error processing the IPN from PayPal. IPN invalid."
 	});
-	return $app->error("ERROR: IPN invalid");
+	$app->response_code( 500 );
+	$app->response_message( 'IPN Validation Failure' );
+	return $out;
     }
-    my $meth = $app->param('txn_type');
-    $logger->debug("Dispatching IPN to $meth()");
-    do { no strict 'refs'; $meth->( $app ); };
 
-    my $out = 'Foo';
+    my $result;
+    my $meth = $app->param('txn_type');
+    do { no strict 'refs'; $result = $meth->( $app ); };
+    if ($result) {
+	$app->response_code( 200 );
+	$app->response_message( 'IPN Successfully Processed' );
+    } else {
+	$app->response_code( 500 );
+	$app->response_message( 'IPN Failure: ' . $app->errstr() );
+    }
     return $out;
 }
 
@@ -98,6 +111,7 @@ sub handle {
 sub adjustment {
     my $app = shift;
     $logger->debug('Processing adjustment');
+    return 1;
 }
 
 # Payment received for multiple items; source is Express Checkout or the PayPal
@@ -105,91 +119,194 @@ sub adjustment {
 sub cart {
     my $app = shift;
     $logger->debug('Processing cart');
+    return 1;
 }
 
 # Payment received for a single item; source is Express Checkout
 sub express_checkout {
     my $app = shift;
     $logger->debug('Processing express_checkout');
-
+    return 1;
 }
 
 # Payment sent using MassPay
 sub masspay {
     my $app = shift;
     $logger->debug('Processing masspay');
+    return 1;
 }
 
 # Monthly subscription paid for Website Payments Pro
 sub merch_pmt {
     my $app = shift;
     $logger->debug('Processing merchant payment');
+    return 1;
 }
 
 # A new dispute was filed
 sub new_case {
     my $app = shift;
     $logger->debug('Processing new dispute');
+    return 1;
 }
 
 # Recurring payment received
 sub recurring_payment {
     my $app = shift;
     $logger->debug('Processing recurring payment');
+    return 1;
 }
 
 # Recurring payment profile created
 sub recurring_payment_profile_created {
     my $app = shift;
     $logger->debug('Processing recurring payment (with profile)');
+    return 1;
 }
 
 # Payment received; source is the Send Money tab on the PayPal website
 sub send_money {
     my $app = shift;
-    $logger->debug('Processing a "send money" request');
+    $logger->debug('Processing a "send money" request, forwarding to web_accept');
+    return _web_accept($app);
 }
 
 # Subscription canceled
 sub subscr_cancel {
     my $app = shift;
     $logger->debug('Processing subscription cancellation');
+    my $id = $app->param('subscr_id');
+    my $subsc = MT->model('subscription')->load({ external_id => $id });
+    unless ($subsc) {
+	MT->log({ message => "Could not find subscription with ID: $id" });
+	return $app->error("Error processing subscription cancellation");
+    }
+    my $pid = $subsc->product_id;
+    my $product = MT->model('asset.product')->load( $pid );
+    unless ($product) {
+	MT->log({ blog_id => $subsc->blog_id,
+		  message => "Warning: could not find product with ID: $pid, cancelling anyway." });
+    }
+    $subsc->status( MT->model('subscription')->CANCELLED() );
+    $subsc->save or return $app->error("Unable to cancel subscription.");;
+    # TODO - fire an email
+    # TODO - fire callbacks
+    return 1;
 }
 
 # Subscription expired
 sub subscr_eot {
     my $app = shift;
     $logger->debug('Processing subscription expired');
+    my $id = $app->param('subscr_id');
+    my $subsc = MT->model('subscription')->load({ external_id => $id });
+    unless ($subsc) {
+	MT->log({ message => "Could not find subscription with ID: $id" });
+	return $app->error("Error processing subscription expiration");
+    }
+    my $pid = $subsc->product_id;
+    my $product = MT->model('asset.product')->load( $pid );
+    unless ($product) {
+	MT->log({ blog_id => $subsc->blog_id,
+		  message => "Warning: could not find product with ID: $pid, expiring anyway." });
+    }
+    $subsc->status( MT->model('subscription')->EXPIRED() );
+    $subsc->save or return $app->error("Unable to expire subscription.");;
+    # TODO - fire an email
+    # TODO - fire callbacks
+    return 1;
 }
 
 # Subscription signup failed
 sub subscr_failed {
     my $app = shift;
     $logger->debug('Processing subscription failure');
+    my $id = $app->param('subscr_id');
+    my $subsc = MT->model('subscription')->load({ external_id => $id });
+    my $pid = $subsc->product_id;
+    unless ($subsc) {
+	MT->log({ message => "Could not find product with ID: $pid" });
+	$subsc = MT->model('subscription')->new;
+	$pid = $app->param( 'item_number' );
+    }
+    my $product = MT->model('asset.product')->load( $pid );
+    unless ($product) {
+	MT->log({ blog_id => $subsc->blog_id,
+		  message => "Warning: could not find product with ID: $pid, expiring anyway." });
+    }
+    unless ($subsc->id) {
+	$subsc->product_id( $pid );
+	$subsc->blog_id( $product->blog_id );
+	$subsc->external_id( $id );
+	$subsc->author_id( $app->param('custom') );
+    }
+    $subsc->status( MT->model('subscription')->FAILURE() );
+    $subsc->save or return $app->error("Unable to expire subscription.");;
+    # TODO - fire an email
+    # TODO - fire callbacks
+    return 1;
 }
 
 # Subscription modified
 sub subscr_modify {
     my $app = shift;
     $logger->debug('Processing subscription modification');
+    # TODO - not currently implemented
+    return 1;
 }
 
 # Subscription payment received
 sub subscr_payment {
     my $app = shift;
     $logger->debug('Processing subscription payment');
+
+    my $id = $app->param('subscr_id');
+    my $subsc = MT->model('subscription')->load({ external_id => $id });
+    unless ($subsc) {
+	MT->log({ message => "Could not find subscription with ID: $id" });
+	return $app->error("Error processing subscription payment");
+    }
+    my $pid = $subsc->product_id;
+    my $product = MT->model('asset.product')->load( $pid );
+    unless ($product) {
+	MT->log({ blog_id => $subsc->blog_id,
+		  message => "Warning: could not find product with ID: $pid." });
+	return $app->error("Product ID $pid not found. Cannot process payment.");
+    }
+    my $payment = _process_payment($app, $product);
+    $payment->subscription_id( $subsc->id );
+    $payment->save or return $app->error("Unable to save payment.");
+    return 1;
 }
 
 # Subscription started
 sub subscr_signup {
     my $app = shift;
     $logger->debug('Processing signup');
+
+    my $pid = $app->param('item_number');
+    my $product = MT->model('asset.product')->load($pid);
+    unless ($product) {
+	MT->log({ message => "Could not find product with ID: $pid" });
+	return $app->error("Error processing subscription sign-up");
+    }
+
+    my $subsc = MT->model('subscription')->new;
+    $subsc->blog_id( $product->blog_id );
+    $subsc->product_id( $product->id );
+    $subsc->author_id( $app->param('custom') );
+    $subsc->external_id( $app->param('subscr_id') );
+    $subsc->status( MT->model('subscription')->ACTIVE() );
+    $subsc->save or return $app->error("Unable to save new subscription.");;
+
+    return 1;
 }
 
 # Payment received; source is Virtual Terminal
 sub virtual_terminal {
     my $app = shift;
-    $logger->debug('Processing payment from virtual terminal');
+    $logger->debug('Processing payment from virtual terminal, forwarding to web_accept');
+    return _web_accept($app);
 }
 
 # Payment received; source is a Buy Now, Donation, or Auction Smart Logos button
@@ -204,18 +321,53 @@ sub web_accept {
 	return $app->error("Error processing payment");
     }
 
+    my $payment = _process_payment($app, $product);
+    $payment->save or return $app->error("Unable to save payment.");;
+    # TODO - fire callback
+    # TODO - save options
+    return 1;
+}
+
+sub _process_payment {
+    my $app = shift;
+    my ($product) = @_;
+    my $txn_id = $app->param('txn_id');
+
+    my $payment = MT->model('payment')->load( { external_transaction_id => $txn_id } );
+    unless ($payment) {
+	$payment = _init_new_payment($app,$product);
+    }
+    if ($payment->payment_status && $payment->payment_status ne $app->param('payment_status')) {
+	# TODO - fire payment status change callbacks
+    }
+    $payment->payment_status( $app->param('payment_status') );
+    if ($payment->payment_status() eq 'Pending') {
+	$payment->is_pending( 1 );
+	MT->log({
+	    blog_id => $payment->blog_id,
+	    message => "Received a PENDING payment: " . _reason_string( $payment->payment_status() )
+	});
+    } elsif ($payment->payment_status() eq 'Completed') {
+	# TODO - fire payment completed callbacks
+    }
+    return $payment;
+}
+
+sub _init_new_payment {
+    my $app = shift;
+    my ($product) = @_;
     my $payment = MT->model('payment')->new;
     $payment->blog_id( $product->blog_id );
     $payment->product_id( $app->param('item_number') );
     $payment->payer_email( $app->param('payer_email') );
     $payment->is_test( $app->param('test_ipn') ? 1 : 0 );
-    $payment->is_pending( $app->param('payment_status') eq 'Pending' );
     $payment->quantity( $app->param('quantity') );
     $payment->external_transaction_id( $app->param('txn_id') );
     $payment->external_payer_id( $app->param('payer_id') );
     $payment->company_name( $app->param('business') );
     $payment->contact_first_name( $app->param('first_name') );
     $payment->contact_last_name( $app->param('last_name') );
+    $payment->address_name( $app->param('address_name') );
     $payment->address_street( $app->param('address_street') );
     $payment->address_city( $app->param('address_city') );
     $payment->address_state( $app->param('address_state') );
@@ -229,16 +381,54 @@ sub web_accept {
     $payment->currency( $app->param('mc_currency') );
     $payment->fee_amount( $app->param('mc_fee') );
     $payment->gross_amount( $app->param('mc_gross') );
-#    $payment->handling_amount( $app->param('') );
     $payment->shipping_amount( $app->param('shipping') );
     $payment->tax( $app->param('tax') );
-    $payment->payment_status( $app->param('payment_status') );
     $payment->payment_type( $app->param('payment_type') );
     $payment->payment_date( $app->param('payment_date') );
-#    $payment->next_payment_date( $app->param('next_payment_date') );
+    return $payment;
+}
 
-    $payment->save or return $app->error("Unable to save payment.");;
-    # TODO - fire callback
+sub _reason_string {
+    my ($code) = @_;
+    if ($code eq 'address') { 
+	return "The payment is pending because your customer did not include a confirmed shipping address and your Payment Receiving Preferences is set yo allow you to manually accept or deny each of these payments. To change your preference, go to the Preferences section of your Profile.";
+    } elsif ($code eq 'authorization') {
+	return "You set the payment action to Authorization and have not yet captured funds.";
+    } elsif ($code eq 'echeck') {
+	return "The payment is pending because it was made by an eCheck that has not yet cleared.";
+    } elsif ($code eq 'intl') {
+	return "The payment is pending because you hold a non-U.S. account and do not have a withdrawal mechanism. You must manually accept or deny this payment from your Account Overview.";
+    } elsif ($code eq 'multi-currency') {
+	return "You do not have a balance in the currency sent, and you do not have your Payment Receiving Preferences set to automatically convert and accept this payment. You must manually accept or deny this payment.";
+    } elsif ($code eq 'order') { 
+	return "You set the payment action to Order and have not yet captured funds.";
+    } elsif ($code eq 'paymentreview') { 
+	return "The payment is pending while it is being reviewed by PayPal for risk.";
+    } elsif ($code eq 'unilateral') { 
+	return "The payment is pending because it was made to an email address that is not yet registered or confirmed.";
+    } elsif ($code eq 'upgrade') {
+	return "The payment is pending because it was made via credit card and you must upgrade your account to Business or Premier status in order to receive the funds. upgrade can also mean that you have reached the monthly limit for transactions on your account.";
+    } elsif ($code eq 'verify') {
+	return "The payment is pending because you are not yet verified. You must verify your account before you can accept this payment.";
+    } elsif ($code eq 'other') {
+	return "Unspecified reason";
+    } elsif ($code eq 'adjustment_reversal') {
+	return "Reversal of an adjustment.";
+    } elsif ($code eq 'buyer-complaint') {
+	return "A reversal has occurred on this transaction due to a complaint about the transaction from your customer.";
+    } elsif ($code eq 'chargeback') {
+	return "A reversal has occurred on this transaction due to a chargeback by your customer.";
+    } elsif ($code eq 'chargeback_reimbursement') {
+	return "Reimbursement for a chargeback";
+    } elsif ($code eq 'chargeback_settlement') {
+	return "Settlement of a chargeback";
+    } elsif ($code eq 'guarantee') {
+	return "A reversal has occurred on this transaction due to your customer triggering a money-back guarantee.";
+    } elsif ($code eq 'refund') {
+	return "A reversal has occurred";
+    } else {
+	return "Unknown reason code: $code.";
+    }
 }
 
 1;
--- plugins/StoreFront/lib/StoreFront/Subscription.pm
+++ plugins/StoreFront/lib/StoreFront/Subscription.pm
@@ -6,16 +6,21 @@ use base qw( MT::Object );
 __PACKAGE__->install_properties({
     column_defs => {
         'id' => 'integer not null auto_increment',
-        'name' => 'string(200) not null',
-        'description' => 'text',
-        'period' => '',           # DAILY, WEEKLY, MONTHLY, YEARLY
-        'duration' => '',         # The number of payments to be made, once per period
-        'amount' => 'float',
-        'subscriber_count' => '', # Cache the number of suscribers
-        'start_date' => '',
+	'blog_id' => 'integer not null',
+	'product_id' => 'integer not null',
+	'author_id' => 'integer not null',
+	'external_id' => 'string(20)',
+	'status' => 'integer not null',
+	'next_payment_due' => 'datetime',
     },
     indexes => {
-#        foo => 1,
+	blog_id => 1,
+	product_id => 1,
+	author_id => 1,
+	status => 1,
+        blog_status => {
+            columns => [ 'blog_id', 'status' ],
+        },
     },
     defaults => {
 #        foo => 0,
@@ -24,12 +29,19 @@ __PACKAGE__->install_properties({
     meta => 0,
     datasource => 'subscription',
     primary_key => 'id',
-    class_type => 'subscription',
 });
 
-# Periods: DAILY, WEEKLY, SEMI_MONTHLY, MONTHLY, EVERY_TWO_MONTHS, QUARTERLY, and YEARLY
-# Display Disposition: specifies when the buyer will be able to access purchased digital content. 
-#    The only valid values for this tag are OPTIMISTIC and PESSIMISTIC.
+sub ACTIVE ()    { 1 }
+sub PENDING ()   { 2 }
+sub EXPIRED ()   { 3 }
+sub CANCELLED () { 4 }
+sub FAILURE ()   { 5 }
+
+use Exporter;
+*import = \&Exporter::import;
+use vars qw( @EXPORT_OK %EXPORT_TAGS);
+@EXPORT_OK = qw( ACTIVE PENDING EXPIRED CANCELLED FAILURE );
+%EXPORT_TAGS = (constants => [ qw(ACTIVE PENDING EXPIRED CANCELLED FAILURE) ]);
 
 1;
 __END__
